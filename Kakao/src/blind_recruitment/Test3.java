61f957b425417a38c53d121c8a899d8f482542f4
chimy2 <premet@naver.com>
2020-09-15 07:33:01 +0900

    solved StringDescendingPlacement

--- Kakao/src/blind_recruitment/Test3.java
+++ Kakao/src/blind_recruitment/Test3.java
@@ -5,6 +5,15 @@ import java.util.regex.*;
 
 public class Test3 {
 	public static void main(String[] args) {
-		
+		Scanner sc=new Scanner(System.in);
+		int r=sc.nextInt();
+		int g=sc.nextInt();
+		int b=sc.nextInt();
+		int i=0;
+		while(i<r*g*b) {
+			
+			System.out.printf("%d %d %d\n",r,i%(i%b+1),i%b);
+			i++;
+		}System.out.println(i);
 	}
 }
--- Practice/src/exercise/StringDescendingPlacement.java
+++ Practice/src/exercise/StringDescendingPlacement.java
@@ -1,17 +1,15 @@
-package blind_recruitment;
+package exercise;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
 
-public class Test4 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String s="sZbddaad";
+public class StringDescendingPlacement {
+    public String solution(String s) {
 		StringBuffer sb=new StringBuffer();
         Integer[] a=new Integer[s.length()];
         for(int i=0;i<s.length();i++) a[i]=s.charAt(i)/1;
         Arrays.sort(a,Collections.reverseOrder());
         for(int i=0;i<a.length;i++) sb.append((char)a[i].intValue());
-        System.out.println(sb);
-	}
+        return sb.toString();
+    }
 }
