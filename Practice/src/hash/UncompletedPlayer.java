7c541bf425ae87fe68fb9df6ff5d598a057c84c8
chimy2 <premet@naver.com>
2020-10-04 03:17:57 +0900

    solved UncompletedPlayer

--- Practice/src/hash/UncompletedPlayer.java
+++ Practice/src/hash/UncompletedPlayer.java
@@ -1,8 +1,24 @@
 package hash;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UncompletedPlayer {
 	public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        return answer;
-    }
+		HashMap<String, Integer> marathon = new HashMap<String, Integer>();
+		for (int i = 0; i < participant.length; i++) {
+			marathon.put(participant[i], getHashValue(marathon, participant[i]) + 1);
+		}
+		for (int i = 0; i < completion.length; i++) {
+			marathon.put(completion[i], getHashValue(marathon, completion[i]) - 1);
+		}
+		// marathon.forEach((k,v) -> { if(v==1) return ""; });
+		for (Map.Entry<String, Integer> entry : marathon.entrySet()) {
+			if (entry.getValue() == 1) return entry.getKey();
+		}
+		return "";
+	}
+	public static int getHashValue(HashMap<String, Integer> hash, String check) {
+		return hash.get(check) == null ? 0 : hash.get(check);
+	}
 }
