a7e3ae8de530c73b18182dd7c21e22babe52566e
chimy2 <premet@naver.com>
2020-10-05 00:41:00 +0900

    solved NumbersOfNWithIntervalX

---
+++ ProblemCollection/src/kakao_blind_recruitment2020/Test2.java
@@ -0,0 +1,19 @@
+package kakao_blind_recruitment2020;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Test2 {
+	public static void main(String[] args) {
+		long begin = System.currentTimeMillis();
+		try {
+		System.out.println();
+		System.err.println();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		//System.out.println((System.currentTimeMillis() - begin) / 1000.0);
+	}
+}
---
+++ ProblemCollection/src/kakao_blind_recruitment2020/Test4.java
@@ -0,0 +1,11 @@
+package kakao_blind_recruitment2020;
+
+import java.net.InetAddress;
+import java.util.Arrays;
+
+public class Test4 {
+	public static void main(String[] args) {
+		
+		System.out.println();
+	}
+}
