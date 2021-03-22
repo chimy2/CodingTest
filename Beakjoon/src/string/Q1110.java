e58e0024bf07eedff3abf7d2035a43fbf8a2540a
chimy2 <premet@naver.com>
2021-03-22 16:10:58 +0900

    solved Q1110

---
+++ Beakjoon/src/string/Q1110.java
@@ -0,0 +1,18 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q1110 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int origin=Integer.parseInt(br.readLine());
+		int num=origin;
+		int answer=0;
+		do {
+			num=(num%10)*10+(num/10+num%10)%10;
+			answer++;
+		}while(origin!=num);
+		System.out.println(answer);
+	}
+}
