d930ea8aacf8f7dc887b3512cd171159ad9a8c96
chimy2 <premet@naver.com>
2021-03-22 17:38:32 +0900

    solved Q2577

---
+++ Beakjoon/src/string/Q2577.java
@@ -0,0 +1,23 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q2577 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int su=1;
+		int[] answer=new int[10];
+		for(int i=0;i<3;i++) {
+			su*=Integer.parseInt(br.readLine());
+		}
+		String s=String.valueOf(su);
+		for(int i=0;i<s.length();i++) {
+			int c=Character.getNumericValue(s.charAt(i));
+			answer[c]++;
+		}
+		for(int i:answer) {
+			System.out.println(i);
+		}
+	}
+}
