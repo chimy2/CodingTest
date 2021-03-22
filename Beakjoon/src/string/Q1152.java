f54eb455116976e5c9ca1e3b5aff9f01e5446dff
chimy2 <premet@naver.com>
2021-03-22 18:58:28 +0900

    solved Q1152

---
+++ Beakjoon/src/string/Q1152.java
@@ -0,0 +1,12 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q1152 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String s=br.readLine().trim();
+        System.out.println(s.length()==0?0:s.split(" ").length);
+    }
+}
