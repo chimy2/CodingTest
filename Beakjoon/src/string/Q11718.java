47a03e6b264697dd42922878e7a11f84d13b3cb5
chimy2 <premet@naver.com>
2021-03-23 00:42:36 +0900

    solved Q11718

---
+++ Beakjoon/src/string/Q11718.java
@@ -0,0 +1,14 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q11718 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String s;
+		while((s=br.readLine())!=null) {
+			System.out.println(s);
+		}
+	}
+}
