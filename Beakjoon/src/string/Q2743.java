c6e6ef5c921a7e4a09c318066384defdfc5de92f
chimy2 <premet@naver.com>
2021-03-25 01:11:33 +0900

    solved Q2743

---
+++ Beakjoon/src/string/Q2743.java
@@ -0,0 +1,10 @@
+package string;
+
+import java.io.*;
+
+public class Q2743 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine().length());
+	}
+}
