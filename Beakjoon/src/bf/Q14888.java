4ad8a69f46a6f1b2a9d7e5591996402545b42ec7
chimy2 <premet@naver.com>
2021-05-11 02:20:19 +0900

    solved Q14888

--- Beakjoon/src/bf/Q14888.java
+++ Beakjoon/src/bf/Q14888.java
@@ -2,7 +2,6 @@ package bf;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Q14888 {
@@ -13,7 +12,6 @@ public class Q14888 {
 		int num=Integer.parseInt(br.readLine());
 		int[] arr=new int[num];
 		int[] count=new int[4];
-		int[] state=new int[num-1];
 		StringTokenizer st=new StringTokenizer(br.readLine());
 		
 		for(int i=0;i<num;i++) {
@@ -24,24 +22,30 @@ public class Q14888 {
 		for(int i=0;i<4;i++) {
 			count[i]=Integer.parseInt(st.nextToken());
 		}
-//		for(int i=0,j=0;i<4&&j<num;) {
-//			if(count[i]==0) i++;
-//			else {
-//				count[i]--;
-//				state[j++]=i;
-//			}
-//		}
+		
 		bf(arr, count, arr[0], 0);
 		System.out.println(max+"\n"+min);
-		System.out.println(Arrays.toString(state));
 	}
+	
 	private static void bf(int[] arr, int[] count, int result, int index) {
-		if(index==arr.length) {
+		if(index==arr.length-1) {
 			if(max<result) max=result;
 			if(min>result) min=result;
 		}
 		for(int i=0;i<4;i++) {
-			
+			if(count[i]>0) {
+				count[i]--;
+				if(i==0) {
+					bf(arr, count, result+arr[index+1], index+1);
+				} else if(i==1) {
+					bf(arr, count, result-arr[index+1], index+1);
+				} else if(i==2) {
+					bf(arr, count, result*arr[index+1], index+1);
+				} else if(i==3) {
+					bf(arr, count, result/arr[index+1], index+1);
+				}
+				count[i]++;
+			}
 		}
 	}
 }
