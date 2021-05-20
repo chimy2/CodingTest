46e8149e53098090f079ce376a3f76a30f0be96a
chimy2 <premet@naver.com>
2021-05-20 23:58:59 +0900

    solving Q11650

--- Beakjoon/src/sort/Q11650.java
+++ Beakjoon/src/sort/Q11650.java
@@ -18,36 +18,47 @@ public class Q11650 {
 	}
 
 	private static void mergeSort(int[][] arr, int start, int end) {
-		if(start+2>end) return;
+//	if(start!=end) {
+		if(start==end) return;
 		int mid=(start+end)/2;
 		mergeSort(arr, start, mid);
 		mergeSort(arr, mid, end);
-		sort(arr, start, mid, end);
+		System.out.printf("start=%d, mid=%d, end=%d\n", start,mid,end);
+//			sort(arr, start, end);
+//			sort(arr, start, mid, end);
+//	}
 	}
 
-	private static void sort(int[][] arr, int start, int mid, int end) {
-		int[][] temp=new int[end-start][2];
-		int l=start, r=mid+1, index=start;
-		
-		while(l<=mid&&r<=end) {
-			if(arr[l][0]<arr[r][0]
-					|| arr[l][0]==arr[r][0] && arr[l][1]<=arr[r][1]) {
-				temp[index++]=arr[l++];
-			} else {
-				temp[index++]=arr[r++];
-			}
-		}
-		
-		while(l<=mid) {
-			temp[index++]=arr[l++];
-		}
-		
-		while(r<=end) {
-			temp[index++]=arr[r++];
-		}
-		
-		for(int i=start;i<end;i++) {
-			arr[i]=temp[i-start];
-		}
+	private static void sort(int[][] arr, int start, int end) {
+		// TODO Auto-generated method stub
+		System.out.printf("%d, %d", start, end);
 	}
+
+//	private static void sort(int[][] arr, int start, int mid, int end) {
+//		int[][] temp=new int[end-start+1][2];
+//		int l=start, r=mid+1, index=0;
+//		System.out.println(l+", "+r);
+//		while(l<=mid||r<=end) {
+//			if(r>end || arr[l][0]<arr[r][0]
+//					|| arr[l][0]==arr[r][0] && arr[l][1]<arr[r][1]) {
+//				System.out.println("1번");
+//				temp[index++]=arr[l++];
+//			} else {
+//				System.out.println("2번");
+//				temp[index++]=arr[r++];
+//			}
+//		}
+//		
+//		while(l<mid) {
+//			temp[index++]=arr[l++];
+//		}
+//		
+//		while(r<end) {
+//			temp[index++]=arr[r++];
+//		}
+//		
+//		for(int i=start;i<=end;i++) {
+//			arr[i]=temp[i];
+//		}
+//	}
 }
