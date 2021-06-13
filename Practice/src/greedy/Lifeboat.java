5523fdc09fab813c499da2e5b9f9f2584d94cfd5
chimy2 <premet@naver.com>
2021-06-13 23:57:00 +0900

    solving 구명보트

--- Practice/src/greedy/Lifeboat.java
+++ Practice/src/greedy/Lifeboat.java
@@ -3,34 +3,28 @@ package greedy;
 import java.util.Arrays;
 
 public class Lifeboat {
-	public static void main(String[] args) {
-		int[] people={70, 50, 80, 50};
-		int limit=100;
-
-        int answer = 0;
-        int min=-1;
+//	90.0 / 100.0
+    public int solution(int[] people, int limit) {
+        float answer=0;
         boolean[] arr=new boolean[people.length];
         Arrays.sort(people);
-        for(int i=0;people[i]<=limit/2 && i<people.length-1;i++) min=i;
-        System.out.println(min);
-        for(int i=people.length-1;i>min;i--) {
-            int index=-1;
-            for(int j=0;j<i;j++) {
-                if(!arr[j]) {
-                    if(people[i]+people[j]>limit) {
-                        break;
-                    } else if(people[i]+people[j]<=limit) {
-                        index=j;
-                    }
-                } 
+        for(int i=people.length-1;i>=0;i--) {
+            if(people[i]<=limit/2) {
+                for(int j=0;j<=i;j++) {
+                    if(!arr[j]) answer+=0.5;
+                }
+                break;
             }
-            if(index!=-1) {
-                arr[index]=true;
-            } 
-            if(!arr[i]) {
-                answer++;
+            for(int j=0;j<=i;j++) {
+                if(!arr[j]) {
+                    if(people[i]+people[j]<=limit) {
+                        arr[j]=true;
+                    }
+                    answer++;
+                    break;
+                }
             }
         }
-        System.out.println(answer+min/2);
-	}
+        return (int) Math.ceil(answer);
+    }
 }
