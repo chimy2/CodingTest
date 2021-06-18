d0a51249f427faf04521beeeb823b13f5abb8a26
chimy2 <premet@naver.com>
2021-06-18 23:02:34 +0900

    solved 주식가격(수정)

--- Practice/src/stack_queue/StockPrice.java
+++ Practice/src/stack_queue/StockPrice.java
@@ -1,10 +1,7 @@
 package stack_queue;
 
-import java.util.Arrays;
-
 public class StockPrice {
-	public static void main(String[] args) {
-		int[] prices= {1, 2, 3, 2, 3};
+    public int[] solution(int[] prices) {
 		int[] answer=new int[prices.length];
 		for(int i=0;i<prices.length;i++) {
 			for(int j=i+1;j<prices.length;j++) {
@@ -14,6 +11,6 @@ public class StockPrice {
 				}
 			}
 		}
-		System.out.println(Arrays.toString(answer));
-	}
+        return answer;
+    }
 }
