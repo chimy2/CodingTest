66eb592e27ca5700fb582250450a1c3ff8a9e40e
chimy2 <premet@naver.com>
2021-06-25 23:52:00 +0900

    solving n진수 게임

--- ProblemCollection/src/kakao_blind_recruitment2018/NNumberGame.java
+++ ProblemCollection/src/kakao_blind_recruitment2018/NNumberGame.java
@@ -1,28 +1,53 @@
 package kakao_blind_recruitment2018;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class NNumberGame {
-	public static void main(String[] args) {
-//		int n=2, t=4, m=2, p=1;
-		int n=16, t=16, m=2, p=1;
-//		n-ì§„ë²•, t-êµ¬í• ê°¯ìˆ˜, m-ê²Œì„ì°¸ê°€ì¸ì›, p-ìˆœì„œ
-		StringBuffer sb=new StringBuffer();
-		List<Character> li=new ArrayList<Character>();
-		for(int i=0,j=0;j<t*m;j++) {
-			int num=j;
-			do {
-				System.out.println(Character.forDigit(num%n, 2));
-				li.add((char) (num%n+48));
-				num/=n;
-			} while(num>0);
-		}
-		for(int i=0;i<t;i++) {
-			sb.append(li.get(i*m+p));
-		}
-		System.out.println(li);
-		System.out.println(sb);
-	}
+   public static void main(String[] args) {
+//      int n=2, t=4, m=2, p=1;
+//      int n=16, t=16, m=2, p=1;
+      int n=3, t=10, m=10, p=1;
+//      n-Áø¹ı, t-±¸ÇÒ°¹¼ö, m-°ÔÀÓÂü°¡ÀÎ¿ø, p-¼ø¼­
+//      Ä­ ¼ö
+      StringBuffer sb=new StringBuffer();
+      char[] arr=new char[t*m];
+      int[] arr2=new int[t*m];
+      int max=1;
+      for(int i=0;i<arr.length;i++) {
+    	  arr2[i]=i;
+      }
+      for(int i=1,j=0;i<t*m;j++) {
+         int num=j;
+         int count=j/n;
+         System.out.println(num+"======");
+         do {
+            if(i+count<=t*m) {
+               arr[i+count-1]=Character.toUpperCase(Character.forDigit(num%n, n));
+//               System.out.println(i+", index="+(i+count)+", "+num+", "+num%n);
+            }
+            System.out.println(count);
+            count--;
+            num/=n;
+         } while(num>0 &&i<t*m);
+//         i+=Math.ceil((float)(j+1)/n);
+         System.out.println(Math.pow(n, max)*max);
+         if(Math.pow(n, max)*max<j+1) {
+        	 max++;
+         }
+         i+=max;
+         
+         System.out.println((int)Math.sqrt(j)+", "+Math.sqrt(j)+", "+j+", "+max);
+         System.out.println(Arrays.toString(arr));
+         System.out.println(Arrays.toString(arr2));
+         System.out.println();
+      }
+      System.out.println(Math.sqrt(9));
+      System.out.println(Arrays.toString(arr));
+      System.out.println(Arrays.toString(arr2));
+      for(int i=0;i<t;i++) {
+         sb.append(arr[i*m+p-1]);
+      }
+//      System.out.println(li);
+      System.out.println(sb);
+   }
 }
