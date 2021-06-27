4ca0a11d08fa3ffee40531ae9ca55b60133c0396
chimy2 <premet@naver.com>
2021-06-27 23:36:01 +0900

    solved n진수 게임

--- ProblemCollection/src/kakao_blind_recruitment2018/NNumberGame.java
+++ ProblemCollection/src/kakao_blind_recruitment2018/NNumberGame.java
@@ -1,49 +1,58 @@
 package kakao_blind_recruitment2018;
 
-import java.util.Arrays;
+import java.util.Stack;
 
 public class NNumberGame {
-	public static void main(String[] args) {  
-//		int n=2, t=4, m=2, p=1;  
-//		int n=16, t=16, m=2, p=1;  
-		int n=3, t=10, m=10, p=1;  
+    public String solution2(int n, int t, int m, int p) {
+		StringBuffer sb=new StringBuffer();
+		Stack<Character> st=new Stack<Character>();
+		char[] arr=new char[t*m];
+		for(int index=0, i=0;index<t*m;i++) {
+			int num=i;
+			do {
+				st.add(Character.toUpperCase(Character.forDigit(num%n, n))); 
+				num/=n;   
+			} while(num>0);  
+			while(!st.isEmpty() && index<t*m) {
+				arr[index++]=st.pop();
+			}
+		}
+		 
+		for(int i=0;i<t;i++) {       
+			sb.append(arr[i*m+p-1]);  
+		}   
+        return sb.toString();
+    }
+	
+//	46.2 / 100.0
+    public String solution(int n, int t, int m, int p) {
 		StringBuffer sb=new StringBuffer();  
 		char[] arr=new char[t*m];  
 		int[] arr2=new int[t*m];  
 		int max=1;  
+		int maxCounter=n;
 		for(int i=0;i<arr.length;i++) {   
 			arr2[i]=i;  
 		}  
-		for(int i=1,j=0;i<t*m;j++) {  
+		for(int i=0,j=0;i<t*m;j++) {  
 			int num=j;  
 			int count=i+max;  
 			do {  
 				if(count<=t*m) {         
 					arr[count-1]=Character.toUpperCase(Character.forDigit(num%n, n));  
-					//               
-					System.out.println(i+"----------, index="+(i+count)+", "+num+", "+num%n);      
 				}      
 				count--;      
 				num/=n;   
 			} while(num>0 &&i<t*m);  
-			//         i+=Math.ceil((float)(j+1)/n);   
-			System.out.println("값="+((Math.pow(n, max)-Math.pow(n, max-1))*max+Math.pow(n, max-1)));   
-//			if(Math.pow(n, max)*max<j+1) {    
-			if(((Math.pow(n, max)-Math.pow(n, max-1))*max+Math.pow(n, max-1))<j+1) {  
-				max++;   
-			}   
-			i+=max;      
-//			System.out.println((int)Math.sqrt(j)+", "+Math.sqrt(j)+", "+j+", "+max);       
-			System.out.println(Arrays.toString(arr));       
-			System.out.println(Arrays.toString(arr2));       
-			System.out.println();    
+			i+=max;   
+			if(maxCounter<=i) {  
+				max++;
+				maxCounter=(int) ((Math.pow(n, max)-Math.pow(n, max-1))*max+Math.pow(n, max-1));
+			}      
 		}    
-		System.out.println(Math.sqrt(9));    
-		System.out.println(Arrays.toString(arr));    
-		System.out.println(Arrays.toString(arr2));    
 		for(int i=0;i<t;i++) {       
 			sb.append(arr[i*m+p-1]);  
 		}        
-		System.out.println(sb);  
-	}
+        return sb.toString();
+    }
 }
