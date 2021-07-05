4353bb8295cf977b1092c231ed680fdd44f49965
chimy2 <premet@naver.com>
2021-07-05 23:46:53 +0900

    solved 베스트앨범

--- Practice/src/hash/BestAlbum.java
+++ Practice/src/hash/BestAlbum.java
@@ -7,28 +7,57 @@ import java.util.List;
 import java.util.Map.Entry;
 
 public class BestAlbum {
-	public static void main(String[] args) {
-		String[] genres= {"classic", "pop", "classic", "classic", "pop"};
-		int[] plays= {500, 600, 150, 800, 2500};
+    public int[] solution(String[] genres, int[] plays) {
+		List<Integer> answer=new ArrayList<Integer>();
 		HashMap<String, Integer> genre=new HashMap<String, Integer>();
-		HashMap<Integer, Album> hm=new HashMap<Integer, Album>();
+		HashMap<Integer, Album> album=new HashMap<Integer, Album>();
+		
 		for(int i=0;i<genres.length;i++) {
-			hm.put(i, new Album(genres[i], plays[i]));
+			album.put(i, new Album(genres[i], plays[i]));
 			genre.put(genres[i], genre.containsKey(genres[i])? 
 					genre.get(genres[i])+plays[i] : plays[i]);
 		}
-		List<Entry<String, Integer>> li=new ArrayList<Entry<String, Integer>>();
+		List<Entry<String, Integer>> li=new ArrayList<>(genre.entrySet());
 		Collections.sort(li, (o1, o2) -> {
-			return o1.getKey().compareTo(o2.getKey());
+			return o2.getValue()-o1.getValue();
 		});
-//		List<Entry<Integer, Album>> li=new ArrayList<Entry<Integer,Album>>();
-//		Collections.sort(li, (o1, o2) -> {
-//			return o1.getKey()-o2.getKey();
-//		});
-		for(Entry<Integer, Album> en:hm.entrySet()) {
-			System.out.println(en.getValue().getGenre());
+        
+		List<Entry<Integer, Album>> albumLi=new ArrayList<>(album.entrySet());
+		Collections.sort(albumLi, (o1, o2) -> {
+			int num=-2;
+			for(int i=0;i<li.size() && num<-1;i++) {
+				boolean b1=o1.getValue().getGenre().equals(li.get(i).getKey());
+				boolean b2=o2.getValue().getGenre().equals(li.get(i).getKey());
+				if(b1 && b2) {
+					num=0;
+				} else if(b1) {
+					num=-1;
+				} else if(b2) {
+					num=1;
+				}
+			}
+			if(num==0) {
+				if(o1.getValue().getPlay()==o2.getValue().getPlay()) {
+					return 0;
+				}
+				return o2.getValue().getPlay()-o1.getValue().getPlay();
+			}
+			return num;
+		});
+        
+		String g=albumLi.get(0).getValue().getGenre();
+		for(int i=0, j=0, k=0;i<album.size();i++, j++) {
+			if(!g.equals(albumLi.get(i).getValue().getGenre())) {
+				g=albumLi.get(i).getValue().getGenre();
+				j=0;
+			}
+			if(j<2) {
+				answer.add(albumLi.get(i).getKey());
+			} 
 		}
+        return answer.stream().mapToInt(i->i).toArray();
 	}
+	
 	public static class Album {
 		String genre;
 		int play;
@@ -48,6 +77,5 @@ public class BestAlbum {
 		public void setPlay(int play) {
 			this.play = play;
 		}
-		
 	}
 }
