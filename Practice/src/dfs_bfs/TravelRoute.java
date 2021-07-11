732658e4f2050965f968fc24975e1a0cd73fdb28
chimy2 <premet@naver.com>
2021-07-11 23:51:09 +0900

    solved 여행경로

--- Practice/src/dfs_bfs/TravelRoute.java
+++ Practice/src/dfs_bfs/TravelRoute.java
@@ -7,10 +7,9 @@ import java.util.List;
 import java.util.Stack;
 
 public class TravelRoute {
-	public static void main(String[] args) {
-//		String[][] tickets={{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}};
-		String[][] tickets={{"ICN", "AOO"}, {"AOO", "BOO"}, {"AOO", "COO"}, {"COO", "AOO"}, {"BOO", "ZOO"}};
-		Stack<String> st=new Stack<String>();
+//	100.0 / 100.0
+    public String[] solution2(String[][] tickets) {
+        Stack<String> st=new Stack<String>();
         boolean[] state=new boolean[tickets.length];
         Arrays.sort(tickets, new Comparator<String[]>() {
             @Override
@@ -21,46 +20,31 @@ public class TravelRoute {
                 return o1[0].compareTo(o2[0]);
             }
         });
-//        for(int i=0;i<tickets.length;i++) {
-//        	System.out.println(Arrays.toString(tickets[i]));
-//        }
         st.add("ICN");
         for(int i=0;i<tickets.length;i++) {
             if(tickets[i][0].equals("ICN")) {
                 st.add(tickets[i][1]);
                 state[i]=true;
-                dfs2(tickets, state, i, st);
+                dfs(tickets, state, st);
+                if(st.size()==tickets.length+1) break;
                 state[i]=false;
                 st.pop();
             }
         }
-        System.out.println(Arrays.toString(st.toArray(String[]::new)));
+        return st.toArray(String[]::new);
     }
     
-    public static void dfs2(String[][] t, boolean[] s, int i, Stack<String> st) {
-    	System.out.print("[");
-    	for(int j=0;j<t.length;j++) {
-	    	System.out.print(Arrays.toString(t[j])+(j!=t.length-1?", ":""));
-	    }
-    	System.out.println("]\n"+Arrays.toString(s));
-    	System.out.println(st+"\n");
-//    	if(st.size()==t.length) {
-//    		System.out.println("오니"+st);
-//    		return;
-//    	} else {
-        st.add(t[i][1]);
+    public static void dfs(String[][] t, boolean[] s, Stack<String> st) {
         for(int j=0;j<t.length;j++) {
-            if(!s[j] && t[j][0].equals(t[i][1])) {
+            if(!s[j] && t[j][0].equals(st.peek())) {
                 s[j]=true;
-                dfs2(t, s, j, st);
-                if(st.size()==t.length) {
-                	return;
-                }
+                st.add(t[j][1]);
+                dfs(t, s, st);
+                if(st.size()==t.length+1) return;
+                st.pop();
                 s[j]=false;
             }
         }
-        st.pop();
-//    	}
     }
 	
 //	50.0 / 100.0
