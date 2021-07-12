cc9b96c53a71dfe9b9cd1c163ab1943eef68c34d
chimy2 <premet@naver.com>
2021-07-13 04:18:26 +0900

    solved 소수 찾기

--- Practice/src/brute_force_search/FindPrimeNumber.java
+++ Practice/src/brute_force_search/FindPrimeNumber.java
@@ -2,76 +2,75 @@ package brute_force_search;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class FindPrimeNumber {
-//	HashSet<Integer> hs=new HashSet<Integer>();
-	public static void main(String[] args) {
-		String numbers="45321";
-//		int[] arr=new int[numbers.length()];
-		String[] arr=numbers.split("");
-		boolean[] state=new boolean[numbers.length()];
-		int len=1;
-//		HashMap<String, Boolean> hm=new HashMap<>();
-		HashSet<String> hs=new HashSet<>();
-		System.out.println(numbers);
-		Arrays.sort(arr);
-		System.out.println(Arrays.toString(arr));
-		hs.add(arrToString(arr));
-		System.out.println(hs);
-//		for(int i=1;i<=numbers.length();i++) {
-//			len*=i;
-//			arr[i-1]=i-1;
-//		}
-//		for(int i=0;i<len;i++) {
-//			sb.setLength(0);
-//			for(int j=0;j<arr.length;j++) {
-//				sb.append(arr[j]);
-//			}
-//			hs.add(Integer.parseInt(sb.toString()));
-//			int index=arr.length-1;
-//			while(index>0 && arr[index-1]<arr[index]) {
-//				state[index]=true;
-//				index--;
-//			}
-//			state[index-1]=true;
-//			for(int j=index-1;j<=state.length;j++) {
-//				if(j==index-1) {
-//					
-//				}
-//			}
-//		}
-		System.out.println(hs);
-	}
-	
-	public static void permutation(String[] arr, HashSet<String> hs) {
-		for(int i=arr.length-2;i>=0;i--) {
-			if(arr[i].compareTo(arr[i+1])<=0) continue;
-			String s="";
-			for(int j=arr.length-1;j>i;j--) {
-				if(arr[i].compareTo(arr[j])!=0) {
-					swap(arr, i, j);
-					break;
-				}
-			}
-			for(int j=i+1;j<arr.length;j++) {
-				swap(arr, j, arr.length-1);
-			}
-			break;
-		}
-		hs.add(arrToString(arr));
-	}
-	
-	public static void swap(String[] arr, int i, int j) {
-		String s=arr[i];
-		arr[i]=arr[j];
-		arr[j]=s;
-	}
-	
-	public static String arrToString(String[] arr) {
-		StringBuffer sb=new StringBuffer();
-		for(int i=0;i<arr.length;i++) {
-			sb.append(arr[i]);
-		}
-		return sb.toString();
-	}
+    public int solution(String numbers) {
+        int answer=0;
+        String[] num=numbers.split("");
+        boolean[] arr=new boolean[(int) Math.pow(10, numbers.length())];
+        HashSet<String> hs=new HashSet<>();
+        HashSet<Integer> result=new HashSet<Integer>();
+
+        Arrays.sort(num);
+        hs.add(arrToString(num));
+        while(permutation(num, hs));
+
+        Iterator<String> it=hs.iterator();
+        while(it.hasNext()) {
+            String s=it.next();
+            for(int i=1;i<=s.length();i++) {
+                result.add(Integer.parseInt(s.substring(0, i)));
+            }
+        }
+
+        Arrays.fill(arr, true);
+        for(int i=0;i<arr.length;i++) {
+            if(i<2) arr[i]=false;
+            else if(arr[i]) {
+                for(int j=(int) Math.pow(i, 2);j<arr.length;j+=i) {
+                    arr[j]=false;
+                }
+            }
+        }
+
+        Iterator<Integer> it2=result.iterator();
+        while(it2.hasNext()) {
+            if(arr[it2.next()]) answer++;
+        }
+        return answer;
+    }
+
+    public static boolean permutation(String[] arr, HashSet<String> hs) {
+        for(int i=arr.length-2;i>=0;i--) {
+            if(arr[i].compareTo(arr[i+1])>=0) continue;
+            for(int j=arr.length-1;j>i;j--) {
+                if(arr[i].compareTo(arr[j])<0) {
+                    swap(arr, i, j);
+                    break;
+                }
+            }
+            int count=arr.length-(arr.length-i-2)/2-1;
+            for(int j=i+1;j<count;j++) {
+                swap(arr, j, arr.length+i-j);
+            }
+            hs.add(arrToString(arr));
+            return true;
+        }
+        return false;
+    }
+
+    public static void swap(String[] arr, int i, int j) {
+        String s=arr[i];
+        arr[i]=arr[j];
+        arr[j]=s;
+    }
+
+    public static String arrToString(String[] arr) {
+        StringBuffer sb=new StringBuffer();
+        for(int i=0;i<arr.length;i++) {
+            sb.append(arr[i]);
+        }
+        return sb.toString();
+    }
 }
