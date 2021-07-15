6aeffc9aabc9ef299d5bc660cdb0b87218ddb5c0
chimy2 <premet@naver.com>
2021-07-15 21:51:16 +0900

    solved [1차] 캐시

--- ProblemCollection/src/kakao_blind_recruitment2018/Cache.java
+++ ProblemCollection/src/kakao_blind_recruitment2018/Cache.java
@@ -8,7 +8,34 @@ import java.util.Map.Entry;
 import java.util.Queue;
 
 public class Cache {
-	//80.0 / 100.0
+//	100.0 / 100.0
+    public int solution2(int cacheSize, String[] cities) {
+        int answer=0;
+        Queue<String> q=new LinkedList<String>();
+
+        for(int i=0;i<cities.length;i++) {
+            cities[i]=cities[i].toLowerCase();
+        }
+        for(int i=0;i<cities.length;i++) {
+            if(cacheSize==0) {
+                answer+=5;
+                continue;
+            } else if(q.contains(cities[i])) {
+                for(int j=0;j<=q.size();j++) {
+                    if(q.peek().equals(cities[i])) q.poll();
+                    else q.add(q.poll());
+                }
+                answer++;
+            } else {
+                if(q.size()==cacheSize) q.poll();
+                answer+=5;
+            }
+            q.add(cities[i]);
+        }
+        return answer;
+    }
+	
+//	80.0 / 100.0
     public int solution(int cacheSize, String[] cities) {
 		Queue<String> citiesQ=new LinkedList<String>();
 		int time=0;
