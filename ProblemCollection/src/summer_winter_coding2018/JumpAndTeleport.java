e460dbee160ae70f6edfced9fd085abed6c4bc18
chimy2 <premet@naver.com>
2021-07-17 17:06:11 +0900

    solved 점프와 순간이동

---
+++ ProblemCollection/src/summer_winter_coding2018/JumpAndTeleport.java
@@ -0,0 +1,16 @@
+package summer_winter_coding2018;
+
+public class JumpAndTeleport {
+    public int solution(int n) {
+        int answer=0;
+		while(n!=0) {
+			if(n%2==0) {
+				n/=2;
+			} else {
+				n--;
+				answer++;
+			}
+		}
+        return answer;
+    }
+}
