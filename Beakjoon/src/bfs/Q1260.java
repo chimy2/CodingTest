d2953294f6d84dc8e5cc59f8c7746703501f29da
chimy2 <premet@naver.com>
2021-07-27 23:42:37 +0900

    solving Q1260

--- Beakjoon/src/bfs/Q1260.java
+++ Beakjoon/src/bfs/Q1260.java
@@ -17,33 +17,32 @@ public class Q1260 {
 		int num3=Integer.parseInt(setting[2]);
 		List<List<Integer>> li=new ArrayList<>();
 		boolean[] check=new boolean[num1];
-		for(int i=0;i<num1;i++) {
+		for(int i=0;i<=num1;i++) {
 			li.add(new ArrayList<Integer>());
 		}
 		for(int i=0;i<num2;i++) {
 			String[] arr=br.readLine().split(" ");
 			int one=Integer.parseInt(arr[0]);
 			int two=Integer.parseInt(arr[1]);
-			li.get(one-1).add(two-1);
-			li.get(two-1).add(one-1);
+			li.get(one).add(two);
+			li.get(two).add(one);
 		}
 		for(int i=0;i<li.size();i++) {
 			Collections.sort(li.get(i));
 		}
-		check[num3-1]=true;
 		System.out.println(li);
-		System.out.print(num3);
-		dfs(li, check, num3-1);
-		System.out.println();
-		dfs2(li, check, num3-1);
+//		System.out.print(num3);
+		dfs(li, check, num3);
 	}
 	
 	public static void dfs(List<List<Integer>> li, boolean[] check, int x) {
+
+		System.out.print(x+" ");
+		check[x]=true;
 		for(int i=0;i<li.get(x).size();i++) {
 			int n=li.get(x).get(i);
 			if(!check[n]) {
-				check[n]=true;
-				System.out.print(" "+(n+1));
+				System.out.println(Arrays.toString(check));
 				dfs(li, check, i);
 			}
 		}
