7ac3ceeb5033f899bb6458fc7bfeb0c881924577
chimy2 <premet@naver.com>
2021-08-03 20:40:23 +0900

    solved 부족한 금액 계산하기

---
+++ ProblemCollection/src/weekly_challenge/CalculateShortfall.java
@@ -0,0 +1,8 @@
+package weekly_challenge;
+
+public class CalculateShortfall {
+    public long solution(int price, int money, int count) {
+        long answer=(long)price*count*(count+1)/2-money;
+        return answer>0 ? answer : 0;
+    }
+}
