62e70db70763b34738d7dbab33231f66e9517da1
chimy2 <premet@naver.com>
2022-02-11 04:31:46 +0900

    solved 문자열 반복해서 출력하기

---
+++ Practice/src/coding_basic_training/RepeatOutputOfString.java
@@ -0,0 +1,13 @@
+package coding_basic_training;
+
+import java.util.Scanner;
+
+public class RepeatOutputOfString {
+//	문자열 반복해서 출력하기
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int n = sc.nextInt();
+		System.out.println(str.repeat(n));
+	}
+}
