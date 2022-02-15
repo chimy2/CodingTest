46616aa8c366e99e515ec43b1cd087d31da9c3f6
chimy2 <premet@naver.com>
2022-02-15 23:13:39 +0900

    solved 문자열 붙여서 출력하기

---
+++ Practice/src/coding_basic_training/PrintConcatenatedStrings.java
@@ -0,0 +1,13 @@
+package coding_basic_training;
+
+import java.util.Scanner;
+
+public class PrintConcatenatedStrings {
+//	문자열 붙여서 출력하기
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+		System.out.println(a + b);
+	}
+}
