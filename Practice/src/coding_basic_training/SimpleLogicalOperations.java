e922bd009c17c3689c3fea2b4778dadc6f5ac940
chimy2 <premet@naver.com>
2022-03-16 17:02:05 +0900

    solved 간단한 논리 연산

---
+++ Practice/src/coding_basic_training/SimpleLogicalOperations.java
@@ -0,0 +1,8 @@
+package coding_basic_training;
+
+public class SimpleLogicalOperations {
+//	간단한 논리 연산
+	public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+		return (x1 | x2) & (x3 | x4);
+	}
+}
