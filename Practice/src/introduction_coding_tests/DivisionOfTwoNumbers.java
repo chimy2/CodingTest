62a8c6565df3a75e6773983da90d12f333af1645
chimy2 <premet@naver.com>
2022-06-17 23:39:39 +0900

    solved 두 수의 나눗셈

---
+++ Practice/src/introduction_coding_tests/DivisionOfTwoNumbers.java
@@ -0,0 +1,8 @@
+package introduction_coding_tests;
+
+public class DivisionOfTwoNumbers {
+//	두 수의 나눗셈
+	public int solution(int num1, int num2) {
+		return (int) ((float) num1 / (float) num2 * 1000);
+	}
+}
