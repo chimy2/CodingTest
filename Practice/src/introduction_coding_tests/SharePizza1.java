64e8c1299e07e4d908824d443d1a3582eeaca6e9
chimy2 <premet@naver.com>
2022-06-25 02:19:41 +0900

    solved 피자 나눠 먹기 (1)

---
+++ Practice/src/introduction_coding_tests/SharePizza1.java
@@ -0,0 +1,9 @@
+package introduction_coding_tests;
+
+public class SharePizza1 {
+//	피자 나눠 먹기 (1)
+	public int solution(int n) {
+		return (n - 1) / 7 + 1;
+//        return (n + 6) / 7;
+	}
+}
---
+++ Practice/src/introduction_coding_tests/SharePizza1.js
@@ -0,0 +1,4 @@
+function solution(n) {
+    return parseInt((n + 6) / 7);
+    // return Math.ceil(n / 7);
+}
