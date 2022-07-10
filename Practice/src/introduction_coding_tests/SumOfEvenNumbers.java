0f96e03a9c9cea52a999539986d29e3c1fd11e2c
chimy2 <premet@naver.com>
2022-07-10 21:17:09 +0900

    solved 짝수의 합

---
+++ Practice/src/introduction_coding_tests/SumOfEvenNumbers.java
@@ -0,0 +1,8 @@
+package introduction_coding_tests;
+
+public class SumOfEvenNumbers {
+//	짝수의 합
+	public int solution(int n) {
+		return n / 2 * (2 + n) / 2;
+	}
+}
---
+++ Practice/src/introduction_coding_tests/SumOfEvenNumbers.js
@@ -0,0 +1,5 @@
+function solution(n) {
+    return ((n / 2) * (2 + n)) / 2;
+    // var half = Math.floor(n / 2);
+    // return half * (half + 1);
+}
