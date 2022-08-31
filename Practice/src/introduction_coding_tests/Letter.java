3e19c92cf2737dd1c6932fa76d02fb7d5487045a
chimy2 <premet@naver.com>
2022-08-31 20:15:53 +0900

    solved 편지

---
+++ Practice/src/introduction_coding_tests/Letter.java
@@ -0,0 +1,8 @@
+package introduction_coding_tests;
+
+public class Letter {
+//	편지
+	public int solution(String message) {
+		return message.length() * 2;
+	}
+}
---
+++ Practice/src/introduction_coding_tests/Letter.py
@@ -0,0 +1,3 @@
+def solution(message):
+    return len(message) * 2
+    # return len(message) << 1
