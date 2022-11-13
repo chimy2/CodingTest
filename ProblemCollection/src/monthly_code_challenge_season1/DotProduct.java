98d4eb04803db914d98aaba4335166215484aea7
chimy2 <premet@naver.com>
2022-11-14 02:59:45 +0900

    solved 내적

--- ProblemCollection/src/monthly_code_challenge_season1/DotProduct.java
+++ ProblemCollection/src/monthly_code_challenge_season1/DotProduct.java
@@ -1,11 +1,15 @@
 package monthly_code_challenge_season1;
 
+import java.util.stream.IntStream;
+
 public class DotProduct {
-    public int solution(int[] a, int[] b) {
-        int answer = 0;
-        for(int i=0;i<a.length;i++) {
-            answer+=a[i]*b[i];
-        }
-        return answer;
-    }
+//	내적
+	public int solution(int[] a, int[] b) {
+		return IntStream.range(0, a.length).reduce(0, (ac, i) -> ac + a[i] * b[i]);
+//		int answer = 0;
+//		for (int i = 0; i < a.length; i++) {
+//			answer += a[i] * b[i];
+//		}
+//		return answer;
+	}
 }
