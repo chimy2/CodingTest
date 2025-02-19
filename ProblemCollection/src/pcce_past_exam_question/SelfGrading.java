cf817a6aa506fcb9bafc872027f22c8138cf64bd
chimy2 <premet@naver.com>
2025-02-19 23:08:06 +0900

    solved [PCCE 기출문제] 6번 / 가채점

---
+++ ProblemCollection/src/pcce_past_exam_question/SelfGrading.java
@@ -0,0 +1,21 @@
+package pcce_past_exam_question;
+
+// [PCCE 기출문제] 6번 / 가채점
+public class SelfGrading {
+
+	public String[] solution(int[] numbers, int[] our_score, int[] score_list) {
+		int num_student = numbers.length;
+		String[] answer = new String[num_student];
+
+		for (int i = 0; i < num_student; i++) {
+//            if (our_score[i] == score_list[i]) {
+			if (our_score[i] == score_list[numbers[i] - 1]) {
+				answer[i] = "Same";
+			} else {
+				answer[i] = "Different";
+			}
+		}
+
+		return answer;
+	}
+}
