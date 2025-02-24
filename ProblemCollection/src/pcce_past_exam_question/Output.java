e58a08132bc02af2416b355acaba92031a5aae62
chimy2 <premet@naver.com>
2025-02-24 23:12:43 +0900

    solved [PCCE 기출문제] 1번 / 출력

---
+++ ProblemCollection/src/pcce_past_exam_question/Output.java
@@ -0,0 +1,15 @@
+package pcce_past_exam_question;
+
+// [PCCE 기출문제] 1번 / 출력
+public class Output {
+
+	public static void main(String[] args) {
+		String msg = "Spring is beginning";
+		int val1 = 3;
+		String val2 = String.valueOf(3);
+
+		System.out.println(msg);
+		System.out.println(val1 + 10);
+		System.out.println(val2 + "10");
+	}
+}
