a924845dbc098720c6d556109e2146cb364d86d7
chimy2 <premet@naver.com>
2025-02-25 22:53:01 +0900

    solved [PCCE 기출문제] 1번 / 문자 출력

---
+++ ProblemCollection/src/pcce_past_exam_question/PrintCharacter.java
@@ -0,0 +1,12 @@
+package pcce_past_exam_question;
+
+// [PCCE 기출문제] 1번 / 문자 출력
+public class PrintCharacter {
+
+	public static void main(String[] args) {
+		String message = "Let's go!";
+
+		System.out.println("3\r\n2\r\n1");
+		System.out.println(message);
+	}
+}
