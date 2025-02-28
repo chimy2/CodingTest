8d63baff040079c586b198cd3f1272f1388091e9
chimy2 <premet@naver.com>
2025-02-28 22:56:42 +0900

    solved [PCCE 기출문제] 4번 / 병과분류

---
+++ ProblemCollection/src/pcce_past_exam_question/DiseaseClassification.java
@@ -0,0 +1,25 @@
+package pcce_past_exam_question;
+
+import java.util.Scanner;
+
+// [PCCE 기출문제] 4번 / 병과분류
+public class DiseaseClassification {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String code = sc.next();
+		String lastFourWords = code.substring(code.length() - 4, code.length());
+
+		if (lastFourWords.equals("_eye")) {
+			System.out.println("Ophthalmologyc");
+		} else if (lastFourWords.equals("head")) {
+			System.out.println("Neurosurgery");
+		} else if (lastFourWords.equals("infl")) {
+			System.out.println("Orthopedics");
+		} else if (lastFourWords.equals("skin")) {
+			System.out.println("Dermatology");
+		} else {
+			System.out.println("direct recommendation");
+		}
+	}
+}
