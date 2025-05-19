90d456746290495fc0e382140b2132947f3dc5a0
chimy2 <premet@naver.com>
2025-05-19 17:25:12 +0900

    solved 조건에 맞는 개발자 찾기

---
+++ Practice/src/sql_select/FindQualifiedDevelopers.sql
@@ -0,0 +1,4 @@
+-- 조건에 맞는 개발자 찾기
+
+-- mysql
+select id, email, first_name, last_name from developers where skill_code & (select sum(code) from skillcodes where name in ('Python', 'C#')) > 0 order by id;
